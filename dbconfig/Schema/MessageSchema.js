bd7a6cca50228ed5b0c6fbe7ea6dc2d70c93a17e
vatchans <112924038+vatchans@users.noreply.github.com>
2023-02-14 20:44:45 +0530

    Update MessageSchema.js

--- dbconfig/Schema/MessageSchema.js
+++ dbconfig/Schema/MessageSchema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const MessageSchema = mongoose.Schema(
   {
     message: {
-      text: { type: String, required: true },
+      text: {required: true },
     },
     users: Array,
     sender: {
