22e16732de491726ffc270f363d24b218d9bd8d3
vatchans <112924038+vatchans@users.noreply.github.com>
2023-02-15 20:12:18 +0530

    Add files via upload

---
+++ src/compontents/map.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect,useState,useRef } from 'react';
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
+import tt from '@tomtom-international/web-sdk-maps';
+function Map() {
+    const mapElement = useRef();
+    const [mapZoom, setMapZoom] = useState(13);
+  
+    useEffect(() => {
+      let data=JSON.parse(localStorage.getItem('location'))
+      console.log(data)
+      let map = tt.map({
+        key: 'mURy0iEpFAt3hNZGAdAQrTrGcJTzoqdD',
+        container: mapElement.current,
+        center: [data.longitude,data.latitude],
+        zoom: 17,
+      });
+      new tt.Marker().setLngLat([data.longitude,data.latitude]).addTo(map)
+      return () => map.remove();
+    });
+  
+    return (
+      
+    <div ref={mapElement} className="mapDiv" />
+    );
+  
+}
+
+export default Map
