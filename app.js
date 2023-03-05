87d9edafd7aed7ba6f98bd02eec01437dee32274
vatchans <112924038+vatchans@users.noreply.github.com>
2023-03-05 13:07:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:3000/",
+      origin: "http://localhost:3000",
       credentials: true,
     },
   });
