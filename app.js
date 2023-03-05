d9ab44101c327a31d234d9993bbfc847d08001b9
vatchans <112924038+vatchans@users.noreply.github.com>
2023-03-05 12:54:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
     cors: {
-      origin: "https://exquisite-selkie-1bc399.netlify.app",
+      origin: "http://localhost:3000/",
       credentials: true,
     },
   });
